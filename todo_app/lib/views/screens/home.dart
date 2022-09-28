5cdfbbff4fd4c7b7cc10d72da456cc5a04babcf4
dimeprog <dimejiawoyefa5@gmail.com>
2022-09-28 02:14:15 +0100

    loading screen with data

--- todo_app/lib/views/screens/home.dart
+++ todo_app/lib/views/screens/home.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_app/providers/todo_provider.dart';
 import 'package:todo_app/views/widgets/color_manager.dart';
 import 'package:todo_app/views/widgets/home_listview.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final data = ref.watch(todoProvider);
     return Scaffold(
       backgroundColor: ColorManager.slowPrimary,
       appBar: AppBar(
@@ -22,7 +26,101 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: const HomeListView(),
+      body: data.when(
+          data: (data) {
+            if (data.isNotEmpty) {
+              return HomeListView(
+                todoList: data,
+              );
+            } else {
+              return const Center(
+                child: Text('No tasks added yet'),
+              );
+            }
+          },
+          error: (error, stackTrace) => Text(error.toString()),
+          loading: () => const Center(child: CircularProgressIndicator())),
+      floatingActionButton: const AddTasksButton(),
+    );
+  }
+}
+
+class AddTasksButton extends ConsumerStatefulWidget {
+  const AddTasksButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ConsumerState<AddTasksButton> createState() => _AddTasksButtonState();
+}
+
+class _AddTasksButtonState extends ConsumerState<AddTasksButton> {
+  TextEditingController _controller = TextEditingController();
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.clear();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      backgroundColor: Colors.teal,
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  title: Center(child: Text('Add New Tasks')),
+                  content: Container(
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _controller,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: const BorderSide(
+                                  color: Colors.lightBlue,
+                                  width: 1,
+                                ),
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Add',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ));
+      },
+      child: const Icon(
+        Icons.add,
+        color: Colors.white,
+      ),
     );
   }
 }
